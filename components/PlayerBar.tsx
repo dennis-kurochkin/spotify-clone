9ec28940ce87cc958e57b94226fd0fc9080e11ba
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2022-07-27 13:35:52 +0300

    feat(Player): add working player

--- components/PlayerBar.tsx
+++ components/PlayerBar.tsx
@@ -1,15 +1,20 @@
 import { Box, Text } from '@chakra-ui/layout'
 import PlayerActions from '~/components/PlayerActions'
+import { useAppSelector } from '~/hooks/useStore'
 import styles from './PlayerBar.module.scss'
 
 const PlayerBar = () => {
+  const activeSong = useAppSelector((state) => state.player.activeSong)
+
   return (
     <Box className={styles.player}>
       <Box>
-        <Box className={styles.songInfoContainer}>
-          <Text className={styles.songName}>Song name</Text>
-          <Text className={styles.artistName}>Artist name</Text>
-        </Box>
+        {!!activeSong && (
+          <Box className={styles.songInfoContainer}>
+            <Text className={styles.songName}>{activeSong.name}</Text>
+            <Text className={styles.artistName}>{activeSong.artist.name}</Text>
+          </Box>
+        )}
       </Box>
       <PlayerActions />
       <Box />
